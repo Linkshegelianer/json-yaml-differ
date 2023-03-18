860faf2f2ff05af482e821533771a4adcd2e014d
Lera <mssolipsismus@gmail.com>
2023-03-18 20:33:37 +0300

    add makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,14 @@
 package hexlet.code;
- class App {
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.io.File;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
+class App {
 
     public static void main(String[] args) {
         System.out.println("hello world");
