e65086caaa305e7793adf9b83efa75232a2abc9f
Lera <mssolipsismus@gmail.com>
2023-03-21 18:16:49 +0300

    add step 4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,15 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
 
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
     @Override
     public void run() {
         System.out.println("doing something");
