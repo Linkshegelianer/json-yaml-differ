c63af1cbc056a512b229ff850e437dabd0f300e2
Lera <mssolipsismus@gmail.com>
2023-05-12 20:03:59 +0300

    add yaml/yml option

--- app/src/main/java/hexlet/code/parser/ParserFactory.java
+++ app/src/main/java/hexlet/code/parser/ParserFactory.java
@@ -3,13 +3,15 @@ package hexlet.code.parser;
 public class ParserFactory {
 
     private static final String JSON_EXTENSION = "json";
-    private static final String YAML_EXTENSION = "yml";
+    private static final String YAML_EXTENSION = "yaml";
+
+    private static final String YML_EXTENSION = "yml";
 
     public static Parser getParser(String extension) {
         Parser parser;
         parser = switch (extension) {
             case JSON_EXTENSION -> new JsonParser();
-            case YAML_EXTENSION -> new YamlParser();
+            case YAML_EXTENSION, YML_EXTENSION -> new YamlParser();
             default -> throw new RuntimeException("File extension '%s' is not supported!".formatted(extension));
         };
         return parser;
