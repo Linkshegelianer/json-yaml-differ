76eb2c3b89e2c238877ee12007affb0d33a405ab
Lera <mssolipsismus@gmail.com>
2023-05-15 12:56:11 +0300

    combine build() and getKeyStatusMap()

--- app/src/main/java/hexlet/code/DiffBuilder.java
+++ app/src/main/java/hexlet/code/DiffBuilder.java
@@ -1,51 +1,45 @@
 package hexlet.code;
 
+
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class DiffBuilder {
 
     public static List<Map<String, Object>> build(Map<String, Object> map1, Map<String, Object> map2) {
         List<Map<String, Object>> diffList = new ArrayList<>();
-        Set<String> keys = new HashSet<>();
+        Set<String> keys = new TreeSet<>(Comparator.naturalOrder());
         keys.addAll(map1.keySet());
         keys.addAll(map2.keySet());
-        keys.stream()
-                .sorted()
-                .forEachOrdered(key -> diffList.add(getKeyStatusMap(key, map1, map2)));
-        return diffList;
-    }
 
-    private static Map<String, Object> getKeyStatusMap(String key, Map<String, Object> map1, Map<String, Object> map2) {
-        Map<String, Object> resultMap = new LinkedHashMap<>();
-        Object oldValue = map1.get(key);
-        Object newValue = map2.get(key);
+        for (String key : keys) {
+            Map<String, Object> resultMap = new LinkedHashMap<>();
+            Object oldValue = map1.get(key);
+            Object newValue = map2.get(key);
 
-        if (map1.containsKey(key) && map2.containsKey(key)) {
-            if (Objects.equals(oldValue, newValue)) {
-                resultMap.put("key", key);
+            resultMap.put("key", key);
+            if (!map1.containsKey(key)) {
+                resultMap.put("status", ParamStatus.ADDED.getStatus());
+                resultMap.put("value", newValue);
+            } else if (!map2.containsKey(key)) {
+                resultMap.put("status", ParamStatus.DELETED.getStatus());
+                resultMap.put("value", oldValue);
+            } else if (Objects.equals(map1.get(key), map2.get(key))) {
                 resultMap.put("status", ParamStatus.UNMODIFIED.getStatus());
                 resultMap.put("value", oldValue);
-            } else {
-                resultMap.put("key", key);
+            } else if (!Objects.equals(map1.get(key), map2.get(key))) {
                 resultMap.put("status", ParamStatus.UPDATED.getStatus());
                 resultMap.put("oldValue", oldValue);
                 resultMap.put("newValue", newValue);
             }
-        } else if (map1.containsKey(key) && !map2.containsKey(key)) {
-            resultMap.put("key", key);
-            resultMap.put("status", ParamStatus.DELETED.getStatus());
-            resultMap.put("value", oldValue);
-        } else if (!map1.containsKey(key) && map2.containsKey(key)) {
-            resultMap.put("key", key);
-            resultMap.put("status", ParamStatus.ADDED.getStatus());
-            resultMap.put("value", newValue);
+            diffList.add(resultMap);
         }
-        return resultMap;
+        return diffList;
     }
 }
